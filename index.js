450f2dd06828b4bd8ab3e70e3a55bbb54d42e812
Camilo Paez <camilopaez.dev@gmail.com>
2022-06-28 21:00:54 -0500

    v2

--- index.js
+++ index.js
@@ -13,8 +13,13 @@ connect(process.env.URI, { useNewUrlParser: true }).then(
 const Answer = model(
   'answer',
   new Schema({
-    name: String,
-    answers: [String]
+    name: Schema.Types.String,
+    answers: Schema.Types.Array,
+    kill: Schema.Types.Boolean,
+    previus: {
+      type: Schema.Types.ObjectId,
+      ref: 'answer'
+    }
   })
 )
 
@@ -23,15 +28,22 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
+app.get('/api/:id', async (req, res) => {
+  const answer = await Answer.findById(req.params.id)
+  
+  res.json(answer)
+})
+
 app.get('/api', async (_, res) => {
   const answer = await Answer.findOne().sort({ field: 'asc', _id: -1 })
   res.json(answer)
 })
 
 app.post('/api', async (req, res) => {
+  console.log({body: req.body})
   const answer = new Answer(req.body)
-  await answer.save()
-  res.send(answer)
+  const savedAnswer = await answer.save()
+  res.json(savedAnswer)
 })
 
 app.listen(process.env.PORT || 3000, () => {
